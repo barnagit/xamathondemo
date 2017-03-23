165832300fd39b75bf1912ef0228210db23af6f0
barnagit <barna.hajzer@gmail.com>
2017-03-23 17:18:31 +0100

    push noti gcm

--- XamathonDemo2/Droid/MainActivity.cs
+++ XamathonDemo2/Droid/MainActivity.cs
@@ -8,6 +8,8 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using Gcm.Client;
+
 namespace XamathonDemo2.Droid
 {
 	[Activity (Label = "XamathonDemo2.Droid",
@@ -19,17 +21,60 @@ namespace XamathonDemo2.Droid
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
-			base.OnCreate (bundle);
+            // Set the current instance of MainActivity.
+            instance = this;
 
-			// Initialize Azure Mobile Apps
-			Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+            base.OnCreate (bundle);
+
+            // Initialize Azure Mobile Apps
+            Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
 
 			// Initialize Xamarin Forms
 			global::Xamarin.Forms.Forms.Init (this, bundle);
 
 			// Load the main application
 			LoadApplication (new App ());
-		}
-	}
+
+            try
+            {
+                // Check to ensure everything's set up right
+                GcmClient.CheckDevice(this);
+                GcmClient.CheckManifest(this);
+
+                // Register for push notifications
+                System.Diagnostics.Debug.WriteLine("Registering...");
+                GcmClient.Register(this, PushHandlerBroadcastReceiver.SENDER_IDS);
+            }
+            catch (Java.Net.MalformedURLException)
+            {
+                CreateAndShowDialog("There was an error creating the client. Verify the URL.", "Error");
+            }
+            catch (Exception e)
+            {
+                CreateAndShowDialog(e.Message, "Error");
+            }
+        }
+
+        private void CreateAndShowDialog(String message, String title)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.SetMessage(message);
+            builder.SetTitle(title);
+            builder.Create().Show();
+        }
+
+        // Create a new instance field for this activity.
+        static MainActivity instance = null;
+
+        // Return the current activity instance.
+        public static MainActivity CurrentActivity
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
 }
 
