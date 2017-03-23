110ee7bd31b8d87814e3d7f3a9e2609937461c72
barnagit <barna.hajzer@gmail.com>
2017-03-23 12:22:45 +0100

    baseline project

---
+++ XamathonDemo2/XamathonDemo2/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace XamathonDemo2
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://xamathondemo2.azurewebsites.net";
+	}
+}
+
