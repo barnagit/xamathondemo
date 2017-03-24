40d9b1503cb990729b49e6e65adc40d5c0690767
tamas.bilik <tamas.bilik@dealogic.com>
2017-03-24 11:36:25 +0100

    Added rating stars to CarouselPage

---
+++ XamathonDemo2/XamathonDemo2/Controls/Extensions/ImageResourceExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InfoFrame.Controls.Extensions
+{
+    [ContentProperty("Source")]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Source != null ? ImageSource.FromResource(Source) : null;
+        }
+    }
+}
